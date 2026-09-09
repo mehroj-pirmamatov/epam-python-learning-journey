7220f2e12e5ed699a9888b61601a0801e9d9364d
Mehroj Pirmamatov <mehrojpirmamatov@gmail.com>
2026-09-09 16:53:54 +0000

    Edit task.py

--- tasks/task.py
+++ tasks/task.py
@@ -1,6 +1,7 @@
 import math
 
-def calculate_expression(a, b):
+
+def some_expression_with_rounding(a, b):
     denominator = 1 + a ** (2 ** b)
     result = (12 * a + 25 * b) / denominator
     return math.ceil(result * 100) / 100
