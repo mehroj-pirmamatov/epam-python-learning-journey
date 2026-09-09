65e57c7720173436c4fd29f82ae171487e3c73c2
Mehroj Pirmamatov <mehrojpirmamatov@gmail.com>
2026-09-09 16:48:59 +0000

    Edit task.py

--- tasks/task.py
+++ tasks/task.py
@@ -1,5 +1,6 @@
 import math
 
-def calculate(a, b):
-    result = (12 * a + 25 * b) / (1 + a**(2**b))
+def calculate_expression(a, b):
+    denominator = 1 + a ** (2 ** b)
+    result = (12 * a + 25 * b) / denominator
     return math.ceil(result * 100) / 100
